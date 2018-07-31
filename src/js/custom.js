1b867e8ff191f95cb676c98270a2356be48328a0
Joel Crawford-Smith <jhc36@duke.edu>
2018-07-31 10:46:32 -0400

    Specify behavior only runs on tabs, not all nav-links

--- src/js/custom.js
+++ src/js/custom.js
@@ -3,10 +3,10 @@
  */
 $(document).ready(function() {
 
-    jQuery('.nav-link').each(function() {
+    jQuery('.nav-tabs .nav-link').each(function() {
       var tabText = jQuery(this).text();
       var tabAriaControlsSelector = '#' + jQuery(this).attr('aria-controls');
-      jQuery(tabAriaControlsSelector).prepend('<a class="a11yTitle sr-only sr-only-focusable" href="' + tabAriaControlsSelector + '">' + tabText + ' tab content</a>');
+      jQuery(tabAriaControlsSelector).prepend('<a class="a11yTitle sr-only" href="' + tabAriaControlsSelector + '">' + tabText + ' tab content</a>');
     });
     jQuery('.nav-link').on('shown.bs.tab', function(e){
       jQuery('.a11yTitle:visible').focus();
