08f0aee173b17ef657b5349cc6a87fdc8bdb2763
Joel Crawford-Smith <jhc36@duke.edu>
2018-07-31 11:00:35 -0400

    Handle focus and activation of tab

--- src/js/custom.js
+++ src/js/custom.js
@@ -6,9 +6,11 @@ $(document).ready(function() {
     jQuery('.nav-tabs .nav-link').each(function() {
       var tabText = jQuery(this).text();
       var tabAriaControlsSelector = '#' + jQuery(this).attr('aria-controls');
-      jQuery(tabAriaControlsSelector).prepend('<a class="a11yTitle sr-only" href="' + tabAriaControlsSelector + '">' + tabText + ' tab content</a>');
+      jQuery(tabAriaControlsSelector)
+        .prepend('<a class="a11yTitle sr-only" href="' + tabAriaControlsSelector + '">' + tabText + ' tab content</a>')
+        .append('<a class="sr-only sr-only-focusable" href="' + tabAriaControlsSelector + '-tab">Back to tab list</a>');
     });
     jQuery('.nav-link').on('shown.bs.tab', function(e){
-      jQuery('.a11yTitle:visible').focus();
+      jQuery('.active .a11yTitle').focus();
     });
   });
