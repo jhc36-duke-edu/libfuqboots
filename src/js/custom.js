c37b763a18a80b96634a68133798032ef0285085
Joel Crawford-Smith <jhc36@duke.edu>
2018-07-31 10:41:05 -0400

    add better keyboard support for tabs

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,3 +1,14 @@
 /*!
  * Your custom javascript
  */
+$(document).ready(function() {
+
+    jQuery('.nav-link').each(function() {
+      var tabText = jQuery(this).text();
+      var tabAriaControlsSelector = '#' + jQuery(this).attr('aria-controls');
+      jQuery(tabAriaControlsSelector).prepend('<a class="a11yTitle sr-only sr-only-focusable" href="' + tabAriaControlsSelector + '">' + tabText + ' tab content</a>');
+    });
+    jQuery('.nav-link').on('shown.bs.tab', function(e){
+      jQuery('.a11yTitle:visible').focus();
+    });
+  });
